0e98918d98a532c7fd76c80a5e84716e7fad8d6f
Muskan <muskankaushik0872@gmail.com>
2021-04-23 15:50:36 +0530

    Frontend

---
+++ src/main/java/com/upgrad/phonedirectory/PhonedirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.phonedirectory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonedirectoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhonedirectoryApplication.class, args);
+    }
+
+}
